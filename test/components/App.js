f129a476909e6b53255e3c4e53ba0111c78f38c6
Dmitriy Kubyshkin <dmitriy.kubyshkin@zalando.de>
2014-10-16 18:34:48 +0200

    Make demo accessible from pages.

--- test/components/App.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-describe('App', function () {
-  it('should create a new instance of WebpackApp', function () {
-    var App = require('scripts/components/App');
-    App.should.be.defined;
-  });
-});
